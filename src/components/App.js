d96c41cda22233a013bad9dda31f0a3ced89876a
Abhimanyu Kolakotla <abhimanyu.kolakotla@merative.com>
2022-07-03 21:51:10 +0530

    Fixed code

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
@@ -9,57 +9,37 @@ const LOCAL_STORAGE_KEY = 'contacts';
 
 function App() {
 
-  const contactJson = [
-      {
-        id:"1",
-        "name": "Vinay",
-        "email": "test@gmail.com",
-      },
-      {
-        id:"2",
-        "name": "Vinay",
-        "email": "test@gmail.com",
-      },
-      {
-        id:"3",
-        "name": "Vinay",
-        "email": "test@gmail.com",
-      },
-      {
-        id:"4",
-        "name": "Vinay",
-        "email": "test@gmail.com",
-      },
-      {
-        id:"5",
-        "name": "Vinay",
-        "email": "test@gmail.com",
-      }
-  ];
+  const [contacts, setContacts] = useState(() => {
+    /**
+     * Initializing State here before render of the component
+     */
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    return retrieveContacts != null ? retrieveContacts : [];
+  });
 
-  const [contacts,setContacts] = useState([]); // created fn component hook with empty array as its value.
-  
-  
   const addContactHandler = (contact) => {
-        console.log('Contact recieved in App comp '+ contact);
-        setContacts([...contacts,contact]);
+    console.log('Contact recieved in App comp ' + contact);
+    setContacts([...contacts, contact]);
   };
 
-  useEffect(() => {
-    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));  
-    if(retrieveContacts) setContacts(retrieveContacts);    
- }, []);
+  /*
+    *********** THIS IS NOT NEEDED ***********
+    useEffect(() => {
+    console.log("Loading existing contacts");
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+  */
 
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));  
- }, [contacts]);
-
-  return ( 
-  <div className='ui container'>
-    <Header />
-    <AddContact addContactHandler={addContactHandler} />
-    <ContactList contacts={contacts} />    
-  </div>
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+  return (
+    <div className='ui container'>
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
+    </div>
   );
 }
 
